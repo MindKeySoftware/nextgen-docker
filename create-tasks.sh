b15c2bf96592302717e6186227e2c327f0c541e5
Boris Wilhelms <mail@wille-zone.de>
2024-06-12 15:25:42 +0200

    feat: create tasks for .net 8

--- create-tasks.sh
+++ create-tasks.sh
@@ -38,3 +38,42 @@ az acr task create \
     --arg BASE="$REGISTRY.azurecr.io/aspnet:6.0-de" \
     --commit-trigger-enabled false \
     --base-image-trigger-name "$REGISTRY.azurecr.io/aspnet:6.0-de"
+
+az acr task create \
+    --registry "$REGISTRY" \
+    --name build-aspnet-8 \
+    --image "aspnet:8.0" \
+    --file 8.0/aspnet.Dockerfile \
+    --context https://github.com/MindKeySoftware/nextgen-docker.git \
+    --commit-trigger-enabled false \
+    --base-image-trigger-name mcr.microsoft.com/dotnet/aspnet:8.0-bookworm-slim
+
+az acr task create \
+    --registry "$REGISTRY" \
+    --name build-aspnet-8-de \
+    --image "aspnet:8.0-de" \
+    --file aspnet-de.Dockerfile \
+    --context https://github.com/MindKeySoftware/nextgen-docker.git \
+    --commit-trigger-enabled false \
+    --arg BASE="$REGISTRY.azurecr.io/aspnet:8.0" \
+    --base-image-trigger-name "$REGISTRY.azurecr.io/aspnet:8.0"
+
+az acr task create \
+    --registry "$REGISTRY" \
+    --name build-aspnet-8-diag \
+    --image "aspnet:8.0-diag" \
+    --file aspnet-diag.Dockerfile \
+    --context https://github.com/MindKeySoftware/nextgen-docker.git \
+    --arg BASE="$REGISTRY.azurecr.io/aspnet:8.0" \
+    --commit-trigger-enabled false \
+    --base-image-trigger-name "$REGISTRY.azurecr.io/aspnet:8.0"
+
+az acr task create \
+    --registry "$REGISTRY" \
+    --name build-aspnet-8-de-diag \
+    --image "aspnet:8.0-de-diag" \
+    --file aspnet-diag.Dockerfile \
+    --context https://github.com/MindKeySoftware/nextgen-docker.git \
+    --arg BASE="$REGISTRY.azurecr.io/aspnet:8.0-de" \
+    --commit-trigger-enabled false \
+    --base-image-trigger-name "$REGISTRY.azurecr.io/aspnet:8.0-de"
