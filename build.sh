fcf29c7636000b4ad07e489bc5d09f489540d1a2
Boris Wilhelms <mail@wille-zone.de>
2024-10-11 11:02:31 +0200

    refactor: remove .net 6 from build

--- build.sh
+++ build.sh
@@ -1,51 +1,28 @@
 #! /bin/sh
 set -ex
 
-# 6.0
-docker build \
-    -t local/aspnet:6.0 \
-    -f 6.0/aspnet.Dockerfile \
-    .
-
-docker build \
-    -t local/aspnet:6.0-de \
-    --build-arg BASE=local/aspnet:6.0 \
-    -f aspnet-de.Dockerfile \
-    .
-
-docker build \
-    -t local/aspnet:6.0-diag \
-    --build-arg BASE=local/aspnet:6.0 \
-    -f aspnet-diag.Dockerfile \
-    .
-
-docker build \
-    -t local/aspnet:6.0-de-diag \
-    --build-arg BASE=local/aspnet:6.0-de \
-    -f aspnet-diag.Dockerfile \
-    .
-
 # 8.0
 
 docker build \
-    -t local/aspnet:8.0 \
-    -f 8.0/aspnet.Dockerfile \
-    .
+  -t local/aspnet:8.0 \
+  -f 8.0/aspnet.Dockerfile \
+  .
 
 docker build \
-    -t local/aspnet:8.0-de \
-    --build-arg BASE=local/aspnet:8.0 \
-    -f aspnet-de.Dockerfile \
-    .
+  -t local/aspnet:8.0-de \
+  --build-arg BASE=local/aspnet:8.0 \
+  -f aspnet-de.Dockerfile \
+  .
 
 docker build \
-    -t local/aspnet:8.0-diag \
-    --build-arg BASE=local/aspnet:8.0 \
-    -f aspnet-diag.Dockerfile \
-    .
+  -t local/aspnet:8.0-diag \
+  --build-arg BASE=local/aspnet:8.0 \
+  -f aspnet-diag.Dockerfile \
+  .
 
 docker build \
-    -t local/aspnet:8.0-de-diag \
-    --build-arg BASE=local/aspnet:8.0-de \
-    -f aspnet-diag.Dockerfile \
-    .
+  -t local/aspnet:8.0-de-diag \
+  --build-arg BASE=local/aspnet:8.0-de \
+  -f aspnet-diag.Dockerfile \
+  .
+
